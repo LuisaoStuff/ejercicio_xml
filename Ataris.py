eb6ee7c97d6bea9e93c11edcdcb2b8da8e7dd3bc
Luis Vazquez Alejo <luisvazquezalejo@gmail.com>
2019-03-05 08:52:52 +0100

    Prueba de xpath y empiezo ejercicio 1

--- Ataris.py
+++ Ataris.py
@@ -11,20 +11,35 @@ ENUNCIADO
 
 	Ejercicio 3
 
-	Filtra los juegos desarrollados por Atari Ink
+	Pide una compañia por teclado, si existe pide un año y muestra los géneros sin repetir que lanzó
+	dicho año. Si no existe muestra un error
+
 
 	Ejercicio 4
 
-	Pide una compañia por teclado, si existe muestra los años en los que desarrolló videojuegos y si no existe muestra un error.
+	Pide una compañia por teclado, si existe muestra los años en los que desarrolló videojuegos y si 
+	no existe muestra un error.
+
+
 
 	Ejercicio 5
 
-	Pide una cadena por teclado. Si hay un juego con ese nombre muestra todos los que tenga la saga (los títulos, edad recomendada e historia si tiene)
+	
+	Pide una cadena por teclado. Si hay un juego con ese nombre muestra todos los que tenga la saga
+	en una lista. A continuación muestra un menú con dichos juegos donde si selecciona alguno muestre:
+		-Título
+		-Género
+		-Edad Recomendada
+		-Historia
 '''
 
 from lxml import etree
 
-def JuegosPorAño()
+def JuegosPorAño(año):
+
+	listajuegos = guia.xpath('//game[year="%s"]/description/text()'%año)
+
+	return listajuegos
 
 guia = etree.parse('Ataris.xml')
 
@@ -32,17 +47,23 @@ while True:
 
 	print('''\n\n	Elige una de las siguientes opciones:
 
-					1. Muestra los juegos desarrollados en un año concreto
-					0. Salir
+		1. Muestra los juegos desarrollados en un año concreto
+		0. Salir
 		''')
 
 	opcion=(int(input("\n		Opción:  ")))
 
 	try:
 
-		if opcion==1:
+		if opcion==0:
 
-			año=int(input())
+			break
+
+		elif opcion==1:
+
+			año=int(input("	Introduce un año concreto: "))
+
+			print(JuegosPorAño(año))
 
 	except:
 		print("\n		",opcion,"no es un entero del menú.")
