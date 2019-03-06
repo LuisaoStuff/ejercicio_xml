dda2e75084858172aefc8fc96c0c7d750ed13748
Luis Vazquez Alejo <luisvazquezalejo@gmail.com>
2019-03-06 09:49:16 +0100

    Ejercicio xml completado

--- Ataris.py
+++ Ataris.py
@@ -1,39 +1,3 @@
-'''
-ENUNCIADO
-
-	Ejercicio 1
-
-	Pide un año por teclado y muestra todos los juegos desarrollados en dicho año
-
-	Ejercicio 2
-
-	Muestra las edades recomendadas y pide una en concreto. A continuación cuenta el número de juegos
-	con dicha edad recomendada.
-
-	Ejercicio 3
-
-	Pide una compañia por teclado tras imprimir una lista de estas. Imprime solo los juegos que tuvieron
-	una calificación por encima de su media. 
-
-	Ejercicio 4
-
-	Muestra una lista de todas las compañias por pantalla. Pide una y enseña los años en los que estuvo
-	activa. A continuación pide uno de esos años e imprime los géneros en los que se enfocó dicho año.
-
-	Ejercicio 5
-
-	
-	Pide una cadena por teclado. Si hay un juego con ese nombre muestra todos los que tenga que ver
-	en una lista. A continuación muestra un menú con dichos juegos donde si selecciona alguno muestre
-	un menú con todos los títulos y que te permita seleccionar cada uno e imprimir:
-		-Consola
-		-Título
-		-Género
-		-Edad Recomendada
-		-Descripcion
-	Hasta que pulses la última opción
-'''
-
 ########################################################################
 #						      Librerías							       #
 ########################################################################
